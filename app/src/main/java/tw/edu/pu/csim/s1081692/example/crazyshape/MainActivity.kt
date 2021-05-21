d08fa0d5a7c05a1bed46b5549b38a8550897b7ea
Username <s1081692@gm.pu.edu.tw>
2021-05-21 14:13:23 +0800

    2021上機考v2_賴侑辰

--- app/src/main/java/tw/edu/pu/csim/s1081692/example/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1081692/example/crazyshape/MainActivity.kt
@@ -4,15 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val img: ImageView = findViewById(R.id.imgTitle)
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(img)
 
         Toast.makeText(baseContext, "作者：賴侑辰", Toast.LENGTH_LONG).show()
 
